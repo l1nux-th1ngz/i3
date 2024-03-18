56cb04368c19185c979d5322d741073deb1372bd
l1nux-th1ngz <137152321+l1nux-th1ngz@users.noreply.github.com>
2024-03-18 08:16:48 -0600

    Update x.sh

--- x.sh
+++ x.sh
@@ -178,29 +178,11 @@ sudo apt install -y osdlyrics
 sudo apt install -y upower
 sudo apt install -y gparted
 sudo apt install -y gnome-disk-utility
-sudo apt install -y task-xfce-desktop
 
-# Create directory with appropriate permissions
-sudo install -d -m 0755 /etc/apt/keyrings
-
-# Import repository signing key
-wget -q https://packages.mozilla.org/apt/repo-signing-key.gpg -O- | sudo tee /etc/apt/keyrings/packages.mozilla.org.asc > /dev/null
-
-# Verify fingerprint
-gpg -n -q --import --import-options import-show /etc/apt/keyrings/packages.mozilla.org.asc | awk '/pub/{getline; gsub(/^ +| +$/,""); if($0 == "35BAA0B33E9EB396F59CA838C0BA5CE6DC6315A3") print "\nThe key fingerprint matches ("$0").\n"; else print "\nVerification failed: the fingerprint ("$0") does not match the expected one.\n"}'
-
-# Add repository
-echo "deb [signed-by=/etc/apt/keyrings/packages.mozilla.org.asc] https://packages.mozilla.org/apt mozilla main" | sudo tee -a /etc/apt/sources.list.d/mozilla.list > /dev/null
-
-# Configure package pinning
-echo '
-Package: *
-Pin: origin packages.mozilla.org
-Pin-Priority: 1000
-' | sudo tee /etc/apt/preferences.d/mozilla
-
-# Update package lists and install Firefox
-sudo apt-get update && sudo apt-get install firefox
+# Wait for Install to complete
+wait
+# Update 
+sudo apt update && sudo apt upgrade -y
 
 sleep 7 &
 PID=$!
@@ -211,7 +193,6 @@ while [ -d /proc/$PID ]
 do
   printf "\b${sp:i++%${#sp}:1}"
 done
-# Wait for Install to complete
-wait
-# Update 
-sudo apt get && sudo apt upgrade -y
+
+# reboot
+sudo reboot
